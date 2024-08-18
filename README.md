a88ff3976d929f6e700db5a2655d6aceb46a21c5
caixiaoshun <3038523973@qq.com>
2024-08-18 18:13:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+
+# Grass segmentation for grass coverage
+
+
+</div>
+
+<br>
+
+
+## TODO
+- 将mmeval 换回torchmetrics
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="GRASSEG",
+    version="0.0.1",
+    description="遥感草地语义分割,测量草地覆盖度",
+    author="",
+    author_email="",
+    url="https://github.com/user/project",
+    install_requires=["lightning", "hydra-core"],
+    packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    entry_points={
+        "console_scripts": [
+            "train_command = src.train:main",
+            "eval_command = src.eval:main",
+        ]
+    },
+)
