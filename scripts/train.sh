398cd05cd7f2cdd5ba40e4463403f431cd3089d8
caixiaoshun <3038523973@qq.com>
2024-09-12 20:58:23 +0800

    feat: 重构训练脚本以支持多模型训练

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,33 @@
-python src/train.py experiment=farseg_resnet18 trainer.devices=[1] logger=wandb
-python src/train.py experiment=farseg_resnet34 trainer.devices=[1] logger=wandb
-python src/train.py experiment=farseg_resnet50 trainer.devices=[1] logger=wandb
-python src/train.py experiment=farseg_resnet101 trainer.devices=[1] logger=wandb
-python src/train.py experiment=fcn trainer.devices=[1] logger=wandb
-python src/train.py experiment=unetmobv2 trainer.devices=[1] logger=wandb
+#!/bin/bash
+
+# 默认参数
+LOGGER="wandb"
+DEVICES="[1]"
+
+# 解析命令行传递的参数
+for arg in "$@"
+do
+    case $arg in
+        logger=*)
+        LOGGER="${arg#*=}"
+        shift
+        ;;
+        devices=*)
+        DEVICES="${arg#*=}"
+        shift
+        ;;
+        *)
+        # 其它未处理的参数
+        ;;
+    esac
+done
+
+# 训练的模型列表
+experiments=("farseg_resnet18" "farseg_resnet34" "farseg_resnet50" "farseg_resnet101" "fcn" "unetmobv2")
+
+# 遍历每个实验配置并运行训练
+for experiment in "${experiments[@]}"
+do
+  echo "开始训练: $experiment 使用 $LOGGER 在设备 $DEVICES"
+  python src/train.py experiment=$experiment trainer.devices=$DEVICES logger=$LOGGER
+done
