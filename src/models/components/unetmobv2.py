a2ff33de526919d90d3299708e76a17503bcb4fc
caixiaoshun <3038523973@qq.com>
2024-09-12 14:58:42 +0800

    feat(models/components): 删除无用代码，优化代码结构

--- src/models/components/unetmobv2.py
+++ src/models/components/unetmobv2.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/8/18 下午4:19
-# @Author  : xiaoshun
-# @Email   : 3038523973@qq.com
-# @File    : unetmobv2.py
-# @Software: PyCharm
 import segmentation_models_pytorch as smp
 import torch
 from torch import nn as nn
