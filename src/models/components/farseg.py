48283d2b32e4a351d8c6da855f2f989af95ae9f3
caixiaoshun <3038523973@qq.com>
2024-09-12 15:02:10 +0800

    feat(farseg): 修改Farseg模型支持多 backbone 选择

--- src/models/components/farseg.py
+++ src/models/components/farseg.py
@@ -1,14 +1,21 @@
 import torch
 from torchgeo.models.farseg import FarSeg as FarSegBase
+from typing import Literal
+
 
 class Farseg(torch.nn.Module):
-    def __init__(self,backbone='resnet50',num_classes=6,backbone_pretrained=True):
+    def __init__(
+        self,
+        backbone: Literal["resnet18", "resnet34", "resnet50", "resnet101"] = "resnet50",
+        num_classes=6,
+        backbone_pretrained=True,
+    ):
         super().__init__()
         self.backbone = FarSegBase(
             backbone=backbone,
             classes=num_classes,
-            backbone_pretrained=backbone_pretrained
+            backbone_pretrained=backbone_pretrained,
         )
-        
+
     def forward(self, x):
         return self.backbone(x)
