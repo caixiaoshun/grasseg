9712a55cdcd1753819207aec370258fc40b8ca69
caixiaoshun <3038523973@qq.com>
2024-09-14 12:56:32 +0800

    feat(models): 更新模型组件以使用新的FCN架构

--- src/models/components/fcn.py
+++ src/models/components/fcn.py
@@ -1,19 +1,45 @@
 import torch
-import torchvision
+from typing import Optional
 from torch import nn as nn
-
-from torchgeo.models.fcn import FCN as _FCN
+import segmentation_models_pytorch as smp
 
 
 class FCN(nn.Module):
     def __init__(
         self,
-        num_classes,
-        in_channels=3,
-        num_filters=64,
+        encoder_name: str = "resnet34",
+        encoder_depth: int = 5,
+        encoder_weights: Optional[str] = "imagenet",
+        decoder_pyramid_channels: int = 256,
+        decoder_segmentation_channels: int = 128,
+        decoder_merge_policy: str = "add",
+        decoder_dropout: float = 0.2,
+        in_channels: int = 3,
+        num_classes: int = 6,
+        activation: Optional[str] = None,
+        upsampling: int = 4,
+        aux_params: Optional[dict] = None,
     ):
         super().__init__()
-        self.backbone = _FCN(in_channels=in_channels, num_filters=num_filters, classes=num_classes)
-
+        self.backbone = smp.FPN(
+            encoder_name=encoder_name,
+            encoder_depth=encoder_depth,
+            encoder_weights=encoder_weights,
+            decoder_pyramid_channels=decoder_pyramid_channels,
+            decoder_segmentation_channels=decoder_segmentation_channels,
+            decoder_merge_policy=decoder_merge_policy,
+            decoder_dropout=decoder_dropout,
+            in_channels=in_channels,
+            classes=num_classes,
+            activation=activation,
+            upsampling=upsampling,
+            aux_params=aux_params,
+        )
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.backbone(x)
+    
+if __name__ == "__main__":
+    x = torch.rand((2, 3, 256, 256))
+    model = FCN()
+    out = model(x)
+    print(out.shape)  # torch.Size([2, 6, 256, 256])
