62628fc8ec0a404bce9b48bce1754c1aed8e0323
caixiaoshun <3038523973@qq.com>
2024-09-17 10:46:19 +0800

    feat: 添加了DeepLabV3模型实现，包括多种预训练模型配置

---
+++ src/models/components/deeplabv3.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn as nn
+import segmentation_models_pytorch as smp
+from typing import Optional, Union,List
+
+
+class DeepLabV3(nn.Module):
+    def __init__(
+        self,
+        encoder_name: str = "resnet34",
+        encoder_depth: int = 5,
+        encoder_weights: Optional[str] = "imagenet",
+        decoder_channels: int = 256,
+        in_channels: int = 3,
+        num_classes: int = 6,
+        activation: Optional[str] = None,
+        upsampling: int = 8,
+        aux_params: Optional[dict] = None,
+    ):
+        super().__init__()
+        self.backbone = smp.DeepLabV3(
+            encoder_name=encoder_name,
+            encoder_depth=encoder_depth,
+            encoder_weights=encoder_weights,
+            decoder_channels=decoder_channels,
+            in_channels=in_channels,
+            classes=num_classes,
+            activation=activation,
+            upsampling=upsampling,
+            aux_params=aux_params,
+        )
+
+    def forward(self, x):
+        return self.backbone(x)
+
+if __name__ == "__main__":
+    x = torch.rand((2, 3, 256, 256))
+    model = DeepLabV3()
+    out = model(x)
+    print(out.shape)  # torch.Size([2, 6, 256, 256])
