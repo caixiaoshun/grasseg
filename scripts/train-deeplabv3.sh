df84bb321dd6f88212f3467dcfd7f489f36424c0
caixiaoshun <3038523973@qq.com>
2024-09-17 11:08:20 +0800

    feat: 更新了训练脚本中的模型配置列表

--- scripts/train-deeplabv3.sh
+++ scripts/train-deeplabv3.sh
@@ -28,7 +28,7 @@ do
 done
 
 # 训练的模型列表
-experiments=("deeplabv3-densenet161" "deeplabv3-dpn131" "deeplabv3-inceptionv4" "deeplabv3-mit_b5" "deeplabv3-mobileone_s4" "deeplabv3-resnet50" "deeplabv3-resnet101" "deeplabv3-resnet152" "deeplabv3-resnext101_32x48d" "deeplabv3-se_resnext101_32x4d" "deeplabv3-timm-efficientnet-l2" "deeplabv3-timm-gernet_l" "deeplabv3-timm-mobilenetv3_large_100" "deeplabv3-timm-regnetx_320" "deeplabv3-timm-res2net101_26w_4s" "deeplabv3-timm-resnest101e")
+experiments=("deeplabv3-dpn131" "deeplabv3-inceptionv4" "deeplabv3-mit_b5" "deeplabv3-mobileone_s4" "deeplabv3-resnet50" "deeplabv3-resnet101" "deeplabv3-resnet152" "deeplabv3-resnext101_32x48d" "deeplabv3-se_resnext101_32x4d" "deeplabv3-timm-efficientnet-l2" "deeplabv3-timm-gernet_l" "deeplabv3-timm-mobilenetv3_large_100" "deeplabv3-timm-regnetx_320" "deeplabv3-timm-res2net101_26w_4s" "deeplabv3-timm-resnest101e")
 
 # 遍历每个实验配置并运行训练
 for experiment in "${experiments[@]}"
