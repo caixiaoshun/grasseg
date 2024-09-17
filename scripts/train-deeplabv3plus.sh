6e3a3942c7c3418b0a95d67120fad0d8dcf67b8d
caixiaoshun <3038523973@qq.com>
2024-09-17 10:51:13 +0800

    feat: 添加了DeepLabV3Plus模型配置文件和训练脚本

---
+++ scripts/train-deeplabv3plus.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 使用示例: bash scripts/train.sh logger=wandb devices="[0,1,2,3]" max_epochs=50
+# 默认参数
+LOGGER="wandb"
+DEVICES="[0]"
+MAX_EPOCHS=150
+
+# 解析命令行传递的参数
+for arg in "$@"
+do
+    case $arg in
+        logger=*)
+            LOGGER="${arg#*=}"
+            ;;
+        devices=*)
+            DEVICES="${arg#*=}"
+            ;;
+        max_epochs=*)
+            MAX_EPOCHS="${arg#*=}"
+            ;;
+        *)
+            # 不支持的参数
+            echo "不支持的参数: $arg"
+            exit 1
+            ;;
+    esac
+done
+
+# 训练的模型列表
+experiments=("deeplabv3plus-densenet161" "deeplabv3plus-dpn131" "deeplabv3plus-inceptionv4" "deeplabv3plus-mit_b5" "deeplabv3plus-mobileone_s4" "deeplabv3plus-resnet50" "deeplabv3plus-resnet101" "deeplabv3plus-resnet152" "deeplabv3plus-resnext101_32x48d" "deeplabv3plus-se_resnext101_32x4d" "deeplabv3plus-timm-efficientnet-l2" "deeplabv3plus-timm-gernet_l" "deeplabv3plus-timm-mobilenetv3_large_100" "deeplabv3plus-timm-regnetx_320" "deeplabv3plus-timm-res2net101_26w_4s" "deeplabv3plus-timm-resnest101e")
+
+# 遍历每个实验配置并运行训练
+for experiment in "${experiments[@]}"
+do
+  echo "开始训练: $experiment 使用 $LOGGER 在设备 $DEVICES"
+  python src/train.py experiment=$experiment trainer.devices=$DEVICES logger=$LOGGER trainer.max_epochs=$MAX_EPOCHS
+done
