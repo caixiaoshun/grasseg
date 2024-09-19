3e7fa29b7c6119c90175b24d9e580fdce7c95e62
caixiaoshun <3038523973@qq.com>
2024-09-19 11:10:31 +0800

    feat(train.sh): 添加脚本用于模型训练

---
+++ scripts/train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 使用示例: bash scripts/train.sh logger=wandb devices="[0,1,2,3]" max_epochs=50
+# 默认参数
+LOGGER="wandb"
+DEVICES="[0]"
+MAX_EPOCHS=150
+
+# 解析命令行传递的参数
+for arg in "$@"
+do
+    case $arg in
+        logger=*)
+            LOGGER="${arg#*=}"
+            ;;
+        devices=*)
+            DEVICES="${arg#*=}"
+            ;;
+        max_epochs=*)
+            MAX_EPOCHS="${arg#*=}"
+            ;;
+        *)
+            # 不支持的参数
+            echo "不支持的参数: $arg"
+            exit 1
+            ;;
+    esac
+done
+
+# 训练的模型列表
+experiments=("fcn-resnet101" "unet-timm-efficientnet-l2	" "pspnet-timm-efficientnet-l2" "fpn-timm-regnetx_320" "linknet-timm-resnest101e" "deeplabv3-resnet152" "deeplabv3plus-timm-efficientnet-l2" "unet_plus_plus-se_resnext101_32x4d" "pan-se_resnext101_32x4d" "manet-se_resnext101_32x4d")
+
+# 遍历每个实验配置并运行训练
+for experiment in "${experiments[@]}"
+do
+  echo "开始训练: $experiment 使用 $LOGGER 在设备 $DEVICES"
+  python src/train.py experiment=$experiment trainer.devices=$DEVICES logger=$LOGGER trainer.max_epochs=$MAX_EPOCHS
+done
