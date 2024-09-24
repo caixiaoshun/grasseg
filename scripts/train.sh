03bc31748f4a0a62c45139850a8ed6b2eb2dab4e
caixiaoshun <3038523973@qq.com>
2024-09-24 16:24:15 +0800

    设置deeplab+的batch size为4

--- scripts/train.sh
+++ scripts/train.sh
@@ -28,7 +28,7 @@ do
 done
 
 # 训练的模型列表
-experiments=("fcn-resnet101" "unet-timm-efficientnet-l2	" "pspnet-timm-efficientnet-l2" "fpn-timm-regnetx_320" "linknet-timm-resnest101e" "deeplabv3-resnet152" "unet_plus_plus-se_resnext101_32x4d" "pan-se_resnext101_32x4d" "manet-se_resnext101_32x4d" "farseg-resnet50")
+experiments=("fcn-resnet101" "unet-timm-efficientnet-l2" "experiment=deeplabv3plus-timm-efficientnet-l2" "pspnet-timm-efficientnet-l2" "fpn-timm-regnetx_320" "linknet-timm-resnest101e" "deeplabv3-resnet152" "unet_plus_plus-se_resnext101_32x4d" "pan-se_resnext101_32x4d" "manet-se_resnext101_32x4d" "farseg-resnet50")
 
 # 遍历每个实验配置并运行训练
 for experiment in "${experiments[@]}"
@@ -36,5 +36,3 @@ do
   echo "开始训练: $experiment 使用 $LOGGER 在设备 $DEVICES"
   python src/train.py experiment=$experiment trainer.devices=$DEVICES logger=$LOGGER trainer.max_epochs=$MAX_EPOCHS
 done
-
-python src/train.py experiment=deeplabv3plus-timm-efficientnet-l2 trainer.devices=$DEVICES logger=$LOGGER trainer.max_epochs=$MAX_EPOCHS data.batch_siz4
