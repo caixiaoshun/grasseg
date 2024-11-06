a729c770c0e449b57fb1fc249b9ca76e55adc097
caixiaoshun <3038523973@qq.com>
2024-11-06 10:31:14 +0800

    更新schedule

--- configs/_base_/schedules/grass_scedule.py
+++ configs/_base_/schedules/grass_scedule.py
@@ -1,15 +1,26 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+optim_wrapper = dict(
+    type="OptimWrapper",
+    optimizer=dict(type="AdamW", lr=0.0006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            "absolute_pos_embed": dict(decay_mult=0.0),
+            "relative_position_bias_table": dict(decay_mult=0.0),
+            "norm": dict(decay_mult=0.0),
+        }
+    ),
+)
 # learning policy
 param_scheduler = [
+    dict(type="LinearLR", start_factor=1e-6, by_epoch=False, begin=0, end=1000),
     dict(
-        type='PolyLR',
-        eta_min=1e-4,
+        type="PolyLR",
+        eta_min=0.0,
         power=0.9,
-        begin=0,
+        begin=1000,
         end=23000,
-        by_epoch=False)
+        by_epoch=False,
+    ),
 ]
 # training schedule for 320k
 train_cfg = dict(
@@ -20,6 +31,6 @@ default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=230, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=230,save_best=['mIoU'],rule=['greater']),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=230,save_best=['mIoU'],rule=['greater'],max_keep_ckpts=1),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='SegVisualizationHook'))
--- configs/swin/swin-tiny-patch4-window7_upernet.py
+++ configs/swin/swin-tiny-patch4-window7_upernet.py
@@ -1,15 +1,18 @@
 _base_ = [
-    '../_base_/models/upernet_swin.py', '../_base_/datasets/grass.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/grass_scedule.py'
+    "../_base_/models/upernet_swin.py",
+    "../_base_/datasets/grass.py",
+    "../_base_/default_runtime.py",
+    "../_base_/schedules/grass_scedule.py",
 ]
 
-norm_cfg = dict(type='BN', requires_grad=True)
+norm_cfg = dict(type="BN", requires_grad=True)
 data_preprocessor = dict(
     size=(256, 256),
-    type='SegDataPreProcessor',
+    type="SegDataPreProcessor",
     mean=[0.4417, 0.5110, 0.3178],
     std=[0.2330, 0.2358, 0.2247],
-    bgr_to_rgb=False)
+    bgr_to_rgb=False,
+)
 
 model = dict(
     data_preprocessor=data_preprocessor,
@@ -21,37 +24,8 @@ model = dict(
         window_size=7,
         use_abs_pos_embed=False,
         drop_path_rate=0.3,
-        patch_norm=True),
+        patch_norm=True,
+    ),
     decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=6),
-    auxiliary_head=dict(in_channels=384, num_classes=6))
-
-# AdamW optimizer, no weight decay for position embedding & layer norm
-# in backbone
-optim_wrapper = dict(
-    _delete_=True,
-    type='OptimWrapper',
-    optimizer=dict(
-        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
-    paramwise_cfg=dict(
-        custom_keys={
-            'absolute_pos_embed': dict(decay_mult=0.),
-            'relative_position_bias_table': dict(decay_mult=0.),
-            'norm': dict(decay_mult=0.)
-        }))
-
-param_scheduler = [
-    dict(
-        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
-    dict(
-        type='PolyLR',
-        eta_min=0.0,
-        power=1.0,
-        begin=1500,
-        end=20000,
-        by_epoch=False,
-    )
-]
-
-train_dataloader = dict(batch_size=4)
-val_dataloader = dict(batch_size=1)
-test_dataloader = val_dataloader
+    auxiliary_head=dict(in_channels=384, num_classes=6),
+)
